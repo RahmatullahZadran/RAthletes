a85c890503745fd2689a90d50d60f901ddad2c5a
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-31 12:28:53 +0100

    added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent} from './navbar/navbar.component';
 import { ShowComponent } from './show/show.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ShowComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, ShowComponent, FooterComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
+  
 })
 export class AppComponent {
   title = 'Rathelete';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ShowComponent } from './show/show.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+   { path: '',
+     component: ShowComponent },
+
+];
