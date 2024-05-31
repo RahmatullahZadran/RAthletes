6167ea871f8f5c70e0cda98b26172313f7eaa032
Rahmatullah <Rahmatullahzadran@outlook.com>
2024-05-31 09:33:01 +0100

    added animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-from-top': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'slide-from-bottom': {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'slide-from-left': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        'slide-from-right': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        'slide-from-top': 'slide-from-top 0.5s ease-out',
+        'slide-from-bottom': 'slide-from-bottom 0.5s ease-out',
+        'slide-from-left': 'slide-from-left 0.5s ease-out',
+        'slide-from-right': 'slide-from-right 0.5s ease-out',
+      },
+    },
   },
   plugins: [],
 }
